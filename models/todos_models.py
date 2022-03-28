ce3a7f96458e9912017e2d0fed100f0090334f4f
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-03-28 20:31:24 +0100

    bug fixed

--- models/todos_models.py
+++ models/todos_models.py
@@ -2,8 +2,8 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class Todo(BaseModel):
-    name: str = Field(min_length=1)
-    description: str= Field(min_length=1, max_length=100)
+    name: str | None= Field(min_length=1)
+    description: str | None= Field(min_length=1, max_length=100)
     completed: Optional[bool]
-    date: str
-    # rate: int = Field(gt = -1, lt = 100)
+    date: str | None
+    rate: int = Field(gt = -1, lt = 100)
